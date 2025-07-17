50a1c66c11b7f6d65e2d3d136ced4d25b7346f05
rizary <rizary@rizilab.com>
2025-07-17 23:29:05 +0700

    add workspace and test

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,9 +1,9 @@
-use rcr::process_tick;
+use package_core::process_tick;
 
 fn main() {
     let mut sum = 0u64;
     for i in 0..1_000_000 {
         sum += process_tick(i);
     }
-    println!("Sum: {}", sum);
+    println!("Sum: {sum}");
 }
